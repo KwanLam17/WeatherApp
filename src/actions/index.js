00b2c0038812dd813dc44e0a47b40b75f43e0776
KwanLam <kwan.lam@alumni.ubc.ca>
2018-08-22 16:52:01 -0700

    First

---
+++ src/actions/index.js
@@ -0,0 +1,18 @@
+import axois from 'axios';
+
+const API_KEY = '1dcf479fb126e5b6a67e6b6ab9788a4d';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+    const url = `${ROOT_URL}&q=${city},us`    // constant country code us = USA
+    const request = axois.get(url);
+
+    //console.log('Request:', request);
+
+    return {
+        type: FETCH_WEATHER,
+        payload: request
+    };
+}
